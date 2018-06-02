588815003f9e6bfc0276a614135af668b447dc83
Jim Gumbley <jgumbley@thoughtworks.com>
2018-06-02 15:29:18 +0100

    open internet egress right up

--- terraform/public.tf
+++ terraform/public.tf
@@ -27,6 +27,20 @@ resource "aws_security_group" "boundary" {
     }
 
   egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port = -1
     to_port = -1
     protocol = "icmp"
